325dc80a223c955c1a77ca9371bf0fc3e85484f8
BAN <ggbbb@mineo.jp>
2021-08-04 02:35:21 +0900

    modify the type of data arrays.

--- src/devices/Bus.java
+++ src/devices/Bus.java
@@ -3,25 +3,25 @@ package devices;
 import org.jetbrains.annotations.NotNull;
 
 public class Bus {
-    Byte[][] romBank; // TODO: impl MBC
-    Byte[] wram;
-    Byte[] vram;
-    Byte[] extVram; // 8KiB
-    Byte[] attributeTable;
+    byte[] romBank; // TODO: impl MBC
+    byte[] wram;
+    byte[] vram;
+    byte[] extVram; // 8KiB
+    byte[] attributeTable;
 
     public Bus(@NotNull final Cartridge cartridge) {
-        romBank = new Byte[2][16 * 1024]; // impl MBC
-        wram = new Byte[8 * 1024]; // 8KiB
-        vram = new Byte[16 * 1024]; // 16KiB
-        extVram = new Byte[8 * 1024]; // 16KiB
-        attributeTable = new Byte[0xA0]; // 0xFE00..0xFE9F分
+        romBank = cartridge.rom;
+        wram = new byte[8 * 1024]; // 8KiB
+        vram = new byte[16 * 1024]; // 16KiB
+        extVram = new byte[8 * 1024]; // 16KiB
+        attributeTable = new byte[0xA0]; // 0xFE00..0xFE9F分
     }
 
     public void write(final int addr, final Byte data) {
         if (addr < 0x4000) {         // ROM bank 0
-            romBank[0][addr] = data;
+            romBank[addr] = data;
         } else if (addr < 0x8000) {  // ROM bank 01~NN
-            romBank[1][addr - 0x4000] = data;
+            romBank[addr - 0x4000] = data;
         } else if (addr < 0xA000) {  // VRAM
             vram[addr - 0x8000] = data;
         } else if (addr < 0xC000) {  // External RAM
@@ -48,9 +48,9 @@ public class Bus {
     public Byte read(int addr) {
         Byte returnVal = 0;
         if (addr < 0x4000) {         // ROM bank 00
-            returnVal = romBank[0][addr];
+            returnVal = romBank[addr];
         } else if (addr < 0x8000) {  // ROM bank 01~NN
-            returnVal = romBank[1][addr - 0x4000];
+            returnVal = romBank[addr - 0x4000];
         } else if (addr < 0xA000) {  // VRAM
             returnVal = vram[addr - 0x8000];
         } else if (addr < 0xC000) {  // External RAM
