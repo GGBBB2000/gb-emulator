086ae294df64ea58c16d50468fb5e945b08dc948
BAN <ggbbb@mineo.jp>
2021-08-09 22:22:17 +0900

    add switchable bank to vram

--- src/Model/VRam.java
+++ src/Model/VRam.java
@@ -1,17 +1,19 @@
 package Model;
 
 class VRam {
-    private final byte[] ram;
+    private final byte[][] ram;
+    private int bank;
 
     public VRam() {
-        this.ram = new byte[16 * 1024]; // 16KiB
+        this.ram = new byte[2][16 * 1024]; // 16KiB * 2
+        this.bank = 0;
     }
 
     public byte read(final int address) {
-        return this.ram[address];
+        return this.ram[this.bank][address];
     }
 
     public void write(final int address, byte data) {
-        this.ram[address] = data;
+        this.ram[bank][address] = data;
     }
 }
