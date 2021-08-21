cc27b2258c7076144dbc72d9cb203e608a68f6ee
BAN <ggbbb@mineo.jp>
2021-08-21 17:20:14 +0900

    add LCD reset

--- src/Model/Lcd.java
+++ src/Model/Lcd.java
@@ -2,25 +2,35 @@ package Model;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.util.Random;
+import java.util.Arrays;
 
 class Lcd {
     byte[] lcd;
     PropertyChangeSupport pcs;
+    boolean isEnabled;
 
     public Lcd(final int width, final int height) {
         this.lcd = new byte[width * height];
         this.pcs = new PropertyChangeSupport(this);
+        this.isEnabled = true;
     }
 
     int pixelCounter = 0;
     public void draw(byte data) {
-        lcd[pixelCounter] = (byte)(new Random()).nextInt();
+        isEnabled = true;
+        lcd[pixelCounter] = data;
         pixelCounter++;
         if (pixelCounter == this.lcd.length) {
             this.pcs.firePropertyChange("LCD_DRAW", false, true);
             this.pixelCounter = 0;
         }
+    }
+
+    public void reset() {
+        Arrays.fill(this.lcd, (byte) 255);
+        this.pcs.firePropertyChange("LCD_DRAW", false, this.isEnabled);
+        this.pixelCounter = 0;
+        this.isEnabled = false;
     }
 
     public byte[] getData() {
